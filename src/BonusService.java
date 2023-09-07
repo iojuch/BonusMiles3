5cacde27ca135275c57bb9fdbb9d95608372e5ef
iojuch <petrovich8384@mail.ru>
2023-09-07 23:19:33 +0300

    Кemoved the bonus limit

--- src/BonusService.java
+++ src/BonusService.java
@@ -3,9 +3,7 @@ public class BonusService {
     public int calculate(int price) {
         int bonus = 20;
         int miles = price / bonus;
-        if (miles > 500) {
-            miles = 500;
-        }
+
         return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         BonusService service = new BonusService();
-        int price = 15_000;
+        int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
 
